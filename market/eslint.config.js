6ccb9dcf3e7698543bbf03a5e01c1d445487f6ef
Yuliya Naletskaya <naletskaya@gmail.com>
2025-05-01 11:42:21 +0300

    feat: download eslint plugins

--- market/eslint.config.js
+++ market/eslint.config.js
@@ -3,9 +3,12 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import eslintReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['node_modules', 'dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -20,13 +23,18 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react: eslintReact,
+      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prefer-const': 'error',
     },
   },
 );
