7dc73335a00ae3e158c80ae41b379eb5c3e63ccc
Yuliya Naletskaya <naletskaya@gmail.com>
2025-05-10 18:52:54 +0300

    feat: add options header to other pages

--- src/components/header/right-menu.tsx
+++ src/components/header/right-menu.tsx
@@ -1,10 +1,19 @@
 import { Button } from '../ui/button/button';
+import { useLocation } from 'react-router-dom';
 
 export function LoginMenu() {
+  const location = useLocation();
+
   return (
     <div className="flex items-center gap-2">
-      <Button variant={'green'}>Log in</Button>
-      <Button>Sing in</Button>
+      {location.pathname === '/' && (
+        <>
+          <Button variant={'green'}>Log in</Button>
+          <Button>Sing in</Button>
+        </>
+      )}
+      {location.pathname === '/login' && <Button>Sign in</Button>}
+      {location.pathname === '/singin' && <Button>Log in</Button>}
       <div>
         <img src="/basket.svg" alt="basket" />
       </div>
