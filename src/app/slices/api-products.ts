03fe530dbf2c9c71efa7a3cc952064d315fb0508
Yuliya Naletskaya <naletskaya@gmail.com>
2025-05-31 21:18:31 +0300

    refactor: change response with refresh token

--- src/app/slices/api-products.ts
+++ src/app/slices/api-products.ts
@@ -1,28 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { baseApiURL, projectKey } from '@/const/api-data';
-import { storage } from '@/service/local-storage';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import type { ProductsResponse } from '@/interfaces/prodactResponse';
+import { baseQueryWithReauth } from './baseQueryWithReauth';
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${baseApiURL}/${projectKey}/graphql`,
-    prepareHeaders: headers => {
-      const token = storage.getData('authToken');
-      console.log(token);
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-        headers.set('Content-Type', 'application/json');
-      }
-
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
 
   endpoints: builder => ({
     getAllProducts: builder.query<ProductsResponse, void>({
       query: () => ({
-        url: '',
+        url: '/graphql',
         method: 'POST',
         body: {
           query: `query {
